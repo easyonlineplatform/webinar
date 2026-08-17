84d03e17193570a238205b3d923db9b7c4af02cd
easyonlineplatform <easyonline2026@gmail.com>
2026-08-17 21:10:35 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -319,7 +319,7 @@ function createBunnyPlayer() {
     iframe.id = "webinarPlayer";
 
     iframe.src =
-    "https://player.mediadelivery.net/embed/722085/d7c65a9b-ed9d-40c1-888f-78865f07f1ed?preload=true&widget=0";
+    "https://player.mediadelivery.net/embed/722085/d7c65a9b-ed9d-40c1-888f-78865f07f1ed?preload=true";
 
     iframe.loading = "lazy";
 
